e600b03f15c095c549b2af84b9063f9e9a1537e7
Sumit Saraf <ssaraf@liveperson.com>
2024-07-11 12:26:01 +0530

    new endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("Listening on port " + PORT))
 app.use(express.json());
 app.get("/getname", (request, response) => {
@@ -14,25 +14,25 @@ app.get("/getname", (request, response) => {
     });
 })
 
-app.post("/updatename", (request, response) => {
-    try {
-        const lastName = request.body.lastName;
-        console.log(lastName);
-        if (!lastName) throw new Error("Last Name not found.");
-        response.status(200).json({
-					statusCode: 1,
-					statusMessage: "SUCCESS",
-					data: {
-						Name: "Sumit "+lastName,
-					},
-				});
-        
-    } catch (error) {
-        response.status(400).json({
-            statusCode: 0,
-            statusMessage: "ERROR",
-            ErrorMessage : error.message
-
-        })
-    }
-})
+app.post("/api/book-appointment", (request, response) => {
+	try {
+		const body = request.body;
+		console.log(body,body.length); 
+		if (!body || Object.keys(body).length == 0)
+			throw new Error("Please provide data to book an appointment.");
+		response.status(200).json({
+			statusCode: 1,
+			statusMessage: "SUCCESS",
+			data: {
+                Message: "Appointment has been Scheduled.",
+                createdTime: new Date().toISOString()
+			},
+		});
+	} catch (error) {
+		response.status(400).json({
+			statusCode: 0,
+			statusMessage: "ERROR",
+			ErrorMessage: error.message,
+		});
+	}
+});
